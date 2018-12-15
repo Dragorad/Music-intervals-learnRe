eaf10d1582fc1a6c13be5f6e0060ac36dcc07577
Dragorad <mi.e.drago@gmail.com>
2018-12-15 13:37:03 +0200

    app with two languages and timers working almost finished

--- src/components/containers/WorkPaneRedux.js
+++ src/components/containers/WorkPaneRedux.js
@@ -1,54 +1,53 @@
-import React, {Component} from 'react'
-import WorkHeader from '../views/workArea/WorkHeader'
+import React, { Component } from 'react'
 import FormSummary from '../views/workArea/FormSummary'
-import Keyboard from '../views/workArea/Keyboard'
 import * as actions from '../../redux/actions/indexActions'
 import jQuery from 'jquery'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import Keyboard0 from '../views/workArea/Keyboard0'
 
 let $ = jQuery
 
 class WorkPaneRedux extends Component {
-
-    componentDidMount() {
-        let that = this
-        let path = $('path')
-
-        function pathClicked() {
-            let targetId = this.id.split('-').join(' - ')
-            console.log(targetId)
-            that.props.setUserAnswer(targetId)
-            $('#testedAnswer').val(targetId)
-            $('.clicked-key').toggleClass()
-            that.props.setUserAnswer(targetId)
-            $(this).toggleClass('clicked-key')
-        }
-
-        path.on('click', pathClicked)
+  
+  componentDidMount () {
+    let that = this
+    let path = $('path')
+    
+    function pathClicked () {
+      let targetId = this.id.split('-').join(' - ')
+      console.log(targetId)
+      that.props.setUserAnswer(targetId)
+      $('#testedAnswer').val(targetId)
+      $('.clicked-key').toggleClass()
+      that.props.setUserAnswer(targetId)
+      $(this).toggleClass('clicked-key')
     }
-
-    render() {
-        let testIntervalData = this.props.testIntervalData
-        let testArr = this.props.testArr
-        return (
-            <React.Fragment>
-                <div className='work-pane'>
-                    <FormSummary/>
-                    <Keyboard/>
-                </div>
-            </React.Fragment>
-
-        )
-    }
+    
+    path.on('click', pathClicked)
+  }
+  
+  render () {
+    let testIntervalData = this.props.testIntervalData
+    let testArr = this.props.testArr
+    return (
+      <React.Fragment>
+        <div className='work-pane'>
+          <FormSummary/>
+          <Keyboard0/>
+        </div>
+      </React.Fragment>
+    
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        testArr: state.testArr,
-        testIntervalData: state.testIntervalData
-    }
+  return {
+    testArr: state.testArr,
+    testIntervalData: state.testIntervalData
+  }
 }
 const mapDispatchToProps = dispach => ({
-    setUserAnswer: userAnswer => dispach(actions.setUserAnswer(userAnswer))
+  setUserAnswer: userAnswer => dispach(actions.setUserAnswer(userAnswer))
 })
 export default connect(mapStateToProps, mapDispatchToProps)(WorkPaneRedux)
